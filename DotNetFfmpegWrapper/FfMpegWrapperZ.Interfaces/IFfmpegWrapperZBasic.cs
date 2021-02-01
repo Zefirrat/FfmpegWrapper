7f5b3b68a58115bef374daeb3c6daa8161173bfc
Zefirrat <leonidpavlov314@gmail.com>
2021-02-01 13:37:58 +0500

    Blank list of this module

--- DotNetFfmpegWrapper/FfMpegWrapperZ.Interfaces/IFfmpegWrapperZBasic.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FfMpegWrapperZ.Interfaces
-{
-    public interface IFfmpegWrapperZBasic
-    {
-        void Concat(string path1, string path2);
-        void Blur(string path);
-        void Imposition(string path);
-    }
-}
